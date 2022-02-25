30b54ec4c19b64b7afbf1219f20110999d076622
Meghan Seger <meghanbmckinney@gmail.com>
2022-02-25 12:47:36 -0600

    more changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,8 @@
 //Where we describe our Restaurants
 const router = require('express').Router()
 
-// More code here in a moment
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
 
 module.exports = router
--- index.js
+++ index.js
@@ -6,24 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
-
-//Wildcard Route (404 route)
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
-
 app.listen(process.env.PORT)
 
-//Import Router
+
+//Wildcard Route (404 route)
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -39,3 +25,4 @@ app.get('*', (req, res) => {
 })
 
 app.listen(process.env.PORT)
+

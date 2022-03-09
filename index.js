f48303bc00b41beed7883216873abf06dcac0e1e
Meghan Seger <meghanbmckinney@gmail.com>
2022-03-08 18:54:13 -0600

    adding default view for layout

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-//Wildcard Route (404 route)
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
+// express settings
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// controllers & routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)

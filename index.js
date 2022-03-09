dad60d89fdf7edbc45a96d53adc033d78582e038
Meghan Seger <meghanbmckinney@gmail.com>
2022-03-08 20:08:57 -0600

    adding homepage view never worked

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const methodOverride = require('method-override')
-
-// express settings
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 
-// controllers & routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+  res.send('Hello world home page')
 })
 
-app.get('*', (req, res) => {
-    res.render('error404')
-})
+app.get('/', (req, res) => {
+    res.render('home')
+  })
 
 app.listen(process.env.PORT)

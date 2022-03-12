438c8d0cbe0e62028f2d54a578d47cc0f53e7b87
Meghan Seger <meghanbmckinney@gmail.com>
2022-03-11 21:22:34 -0600

    multiple changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,19 @@
-//Where we describe our Restaurants
-const router = require('express').Router()
-
+const router = require("express").Router();
 // GET /places
-app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-    }]
-
-res.render('places/index', { places })
-
-module.exports = router
+router.get('/', (req, res) => {
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: "/images/florencia-viadana--2etgsqvy68-unsplash.jpg"
+  }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: "/images/marcos-paulo-prado-fdzsoySp9Uo-unsplash.jpg"
+  }]
+      res.render('places/index', { places })
+  })
+module.exports = router;
